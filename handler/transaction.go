f54c19f23a5a1c9d9a54f9f62458793218312eee
ekawibawaputu <ekawibawaputu@gmail.com>
2023-10-13 03:11:48 +0800

    Get transactions endpoint

---
+++ handler/transaction.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/transaction"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHanlder(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign`s transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign`s transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign`s transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
+// parameter di uri
+// tangkap paramter mapping input struct
+// didalam handler panggil service, input sebagai parameter
+// dalam service, berbekal campaign id bisa panggil repo
+// repo mencari data transaction suatu campaign
---
+++ transaction/service.go
@@ -0,0 +1,40 @@
+package transaction
+
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository ) *service {
+	return &service{repository, campaignRepository}
+
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+
+	// get campaign
+	// check campaign userId != user_id yang melakukan request
+	campaign, err := s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("not an owner of campaign")
+	}
+
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
